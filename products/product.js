1ba9035585f691c1a025cd0a1f0966127802a41a
IdaDanuartha <danuart21@gmail.com>
2023-12-11 23:33:05 +0800

    static method

--- products/product.js
+++ products/product.js
@@ -59,24 +59,52 @@ const productSchema =new mongoose.Schema({
   },
 })
 
+productSchema.methods.outStock = function() {
+  this.stock = 0
+  return this.save()
+}
+
+productSchema.statics.closeStore = function() {
+  return this.updateMany({}, {
+    stock: 0,
+    "availability.online": false,
+    "availability.offline": false
+  })
+}
+
 const Product = mongoose.model('Product', productSchema)
 
-// Product.insertMany(products) 
-Product.findOneAndUpdate({name: 'Kemeja Flanel'}, {
-  "name": "Kemeja Flanel",
-  "brand": "Hollister",
-  "price": 750000,
-  "color": "biru muda",
-  "size": ["S", "M", "L"],
-  "description": "Kemeja flanel dengan warna yang cerah, terbuat dari bahan flanel yang nyaman dan berkualitas tinggi.",
-  "condition": "baru",
-  "stock": 25,
-  "availability": {
-    "online": true,
-    "offline": true
-  }
-}, {new: true, runValidators: true}).then(result => {
+const changeStock = async (id) => {
+  const foundProduct = await Product.findById(id)
+  await foundProduct.outStock()
+  console.log("Stock updated successfully")
+}
+
+// changeStock('657728953dc07b619cf1267d')
+
+Product.closeStore().then((result) => {
   console.log(result)
-}).catch(err => {
-  console.error(err.errors.stock.properties.message) 
+}).catch((err) => {
+  console.error(err)
 })
+
+
+// Product.insertMany(products) 
+// Product.findOneAndUpdate({name: 'Kemeja Flanel'}, {
+//   "name": "Kemeja Flanel",
+//   "brand": "Hollister",
+//   "price": 750000,
+//   "color": "biru muda",
+//   "size": ["S", "M", "L"],
+//   "description": "Kemeja flanel dengan warna yang cerah, terbuat dari bahan flanel yang nyaman dan berkualitas tinggi.",
+//   "condition": "baru",
+//   "stock": 25,
+//   "availability": {
+//     "online": true,
+//     "offline": true
+//   }
+// }, {new: true, runValidators: true}).then(result => {
+//   console.log(result)
+// }).catch(err => {
+//   console.error(err.errors.stock.properties.message) 
+// })
